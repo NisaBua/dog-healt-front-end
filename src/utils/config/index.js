aeabbcd42b163a2cdbb7a99c87a02957235968e5
NisaBua <nisabualuang@gmail.com>
2022-07-29 23:41:41 +0700

    add detailver2

--- src/utils/config/index.js
+++ src/utils/config/index.js
@@ -1,10 +1,8 @@
 export const servicePath = {
     base_url: process.env.REACT_APP_RUN_ON === 'production' ?
-        'http://localhost:3000' :
-        'http://localhost:3000',
+        'http://localhost:3000' : 'http://localhost:3000',
     service: {
         general: process.env.REACT_APP_RUN_ON === 'production' ?
-            'https://dog-healt-service.herokuapp.com/' :
-            'http://localhost:4020'
+            'https://dog-healt-service.herokuapp.com' : 'http://localhost:4020'
     }
 }
